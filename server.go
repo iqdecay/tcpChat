7449d96c673c39473e4aa9c0142a521e9e54a9cb
vnepveu <victor.nepveu@imt-atlantique.net>
2019-01-25 19:35:01 +0100

    Renaming

---
+++ server.go
@@ -0,0 +1,169 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"reflect"
+	"regexp"
+	"strings"
+)
+
+type Client struct {
+	name string
+	id   int
+}
+
+type Server struct {
+	allClients map[net.Conn]Client
+	allPseudo  []string
+	totalClients int
+	connectedClients int
+	listener net.Listener
+}
+
+var validPseudo = regexp.MustCompile(`([A-Z]|[a-z]|[0-9]){4,12}`)
+
+func getValidPseudo(conn net.Conn) string {
+	conn.Write([]byte("\n Please enter a new pseudo : "))
+	reader := bufio.NewReader(conn)
+	pseudo, _ := reader.ReadString('\n')
+	pseudo = strings.Trim(pseudo, "\n")
+	for !validPseudo.MatchString(pseudo) {
+		conn.Write([]byte("Pseudo are alphanumerical and of length in [4,12]"))
+		conn.Write([]byte("Please enter a new pseudo : "))
+		pseudo, _ := reader.ReadString('\n')
+		pseudo = strings.Trim(pseudo,"\n")
+	}
+	return pseudo
+}
+
+func disconnect(conn net.Conn, server *Server) {
+	client := server.allClients[conn]
+	pseudo := client.name
+	i := find(server.allPseudo, pseudo)
+	server.allPseudo = append(server.allPseudo[:i],server.allPseudo[i+1:]...)
+	server.connectedClients--
+	conn.Close()
+	delete(server.allClients, conn)
+	log.Printf("Client with pseudo %s disconnected", pseudo)
+	log.Printf("There are %d connected clients", server.connectedClients)
+}
+
+func find(s interface{}, elem interface{}) int {
+	// Return -1 if elem is in s, its index in s otherwise
+	arrV := reflect.ValueOf(s)
+	if arrV.Kind() == reflect.Slice {
+		for i := 0; i < arrV.Len(); i++ {
+			if arrV.Index(i).Interface() == elem {
+				return i
+			}
+		}
+	}
+	return -1
+
+}
+func contains(s interface{}, elem interface{}) bool {
+	// Return true if elem is in s, false otherwise
+	arrV := reflect.ValueOf(s)
+	if arrV.Kind() == reflect.Slice {
+		for i := 0; i < arrV.Len(); i++ {
+
+			// panics if slice element points to an unexported struct field
+			if arrV.Index(i).Interface() == elem {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func main() {
+	server := new(Server)
+	server.allClients = make(map[net.Conn]Client)
+	// TCP will push new connections to it
+	newConnections := make(chan net.Conn)
+	// We will remove those clients from allClients
+	deadConnections := make(chan net.Conn)
+	// Channel which will contain message from connected clients
+	messages := make(chan string)
+	// Start TCP server
+	var err error
+	server.listener, err = net.Listen("tcp", ":6060")
+	if err != nil {
+		err = fmt.Errorf("error launching the server : %e", err)
+		fmt.Println(err)
+	}
+
+	// Server accepts connections forever and pushes new ones
+	// to the dedicated channel
+	go func() {
+		for {
+			conn, err := server.listener.Accept()
+			if err != nil {
+				fmt.Println(err)
+			}
+			newConnections <- conn
+		}
+
+	}()
+
+	// Infinite loop
+	for {
+		select {
+
+		// Continuously accept new clients
+		case conn := <-newConnections:
+			server.totalClients++
+			log.Printf("Accepted new client with id %d", server.totalClients)
+			// Read all incoming messages from this client into a goroutine
+			// and push them to the message chan
+			go func(conn net.Conn, server *Server) {
+				reader := bufio.NewReader(conn)
+				conn.Write([]byte("Welcome to the server ! \n"))
+				pseudo := getValidPseudo(conn)
+				for contains(server.allPseudo, pseudo) {
+					conn.Write([]byte("Pseudo already in use, please choose a new one"))
+					pseudo = getValidPseudo(conn)
+				}
+				conn.Write([]byte(fmt.Sprintf("Your pseudo is now %s \n", pseudo)))
+				client := Client{pseudo, server.totalClients}
+				server.allClients[conn] = client
+				server.allPseudo = append(server.allPseudo, pseudo)
+				server.connectedClients++
+				reader = bufio.NewReader(conn)
+				for {
+
+					incoming, err := reader.ReadString('\n')
+					if err != nil {
+						break
+					}
+					messages <- fmt.Sprintf("%s > %s", client.name, incoming)
+				}
+				// When encounter an error, the client will be removed
+				deadConnections <- conn
+
+			}(conn, server)
+
+
+		// Continuously read incoming messages and broadcast them
+		case message := <-messages:
+			for conn := range server.allClients {
+				//Send the message in a go routine
+				go func(conn net.Conn, message string) {
+					_, err := conn.Write([]byte(message))
+					// If it doesn't work the connection is dead
+					if err != nil {
+						deadConnections <- conn
+					}
+				}(conn, message)
+			}
+			log.Printf("New message : %s", message)
+
+		//Remove dead clients
+		case conn := <-deadConnections:
+			disconnect(conn, server)
+		}
+	}
+}
