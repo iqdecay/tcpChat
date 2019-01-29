85b2802e9ab5816dad971c4953c03f5641c02ade
vnepveu <victor.nepveu@imt-atlantique.net>
2019-01-29 15:52:13 +0100

    New ui

--- client.go
+++ client.go
@@ -3,30 +3,94 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"github.com/marcusolsson/tui-go"
-	"log"
+	"github.com/jroimartin/gocui"
+	"io"
 	"net"
 	"time"
 )
 
+var keystrokes = make(chan gocui.Key)
 
-func receiveMessage(conn net.Conn, history *tui.Box) {
+func editor(v *gocui.View, key gocui.Key, ch rune, mod gocui.Modifier) {
+	keystrokes <- key
+}
+
+func layout(g *gocui.Gui) error {
+	maxX, maxY := g.Size()
+	if v, err := g.SetView("users", 0, 0, maxX/4, maxY-1); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		v.Title = "Users"
+		fmt.Println("")
+		fmt.Println(" victor")
+		fmt.Println(" jean")
+		fmt.Println(" mano")
+	}
+
+	if v, err := g.SetView("chat", maxX/4+1, 0, maxX-1, 3*maxY/4-1); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		if _, err := g.SetCurrentView("chat"); err != nil {
+			return err
+		}
+		v.Title = "Chat"
+		v.Autoscroll = true
+		v.Overwrite = false
+		v.Wrap = true
+
+	}
+
+	if v, err := g.SetView("input", maxX/4+1, 3*maxY/4, maxX-1, maxY-1); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		if _, err := g.SetCurrentView("input"); err != nil {
+			return err
+		}
+		v.Editable = true
+		v.Wrap = true
+	}
+	return nil
+
+}
+
+func quit(g *gocui.Gui, v *gocui.View) error {
+	return gocui.ErrQuit
+}
+
+func displayMessage(g *gocui.Gui, viewName string, message []byte) error {
+	message = append([]byte(time.Now().Format("15:04")+">"), message...)
+	originalView := g.CurrentView()
+	g.SetCurrentView(viewName)
+	v := g.CurrentView()
+	v.Write(message)
+	g.SetCurrentView(originalView.Name())
+	return nil
+}
+
+func initKeyBindings(g *gocui.Gui) error {
+	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+		return err
+	}
+	return nil
+}
+func receiveMessage(conn net.Conn, g *gocui.Gui) {
 	reader := bufio.NewReader(conn)
 	for {
 		incoming, err := reader.ReadString('\n')
 		if err != nil {
 			break
 		}
-		history.Append(tui.NewHBox(
-			tui.NewLabel(time.Now().Format("15:04")),
-			tui.NewPadder(1, 0, tui.NewLabel(fmt.Sprintf("<%s>", "john"))),
-			tui.NewLabel(incoming),
-			tui.NewSpacer(),
-		))
+		message := []byte(incoming)
+		displayMessage(g, "chat", message)
+		g.Update(func(g *gocui.Gui) error {
+			return nil
+		})
 	}
 
 }
-
 func main() {
 	//Initialize connection to chat server
 	conn, err := net.Dial("tcp", ":6060")
@@ -35,50 +99,51 @@ func main() {
 		fmt.Println(err)
 	}
 	defer conn.Close()
-
-	// Initialize UI
-	userList := tui.NewList()
-	sidebar := tui.NewVBox(
-		tui.NewLabel("Users"),
-		userList)
-	sidebar.SetBorder(true)
-
-	history := tui.NewVBox()
-
-	historyScroll := tui.NewScrollArea(history)
-	historyScroll.SetAutoscrollToBottom(true)
-
-	historyBox := tui.NewVBox(historyScroll)
-	historyBox.SetBorder(true)
-
-	input := tui.NewEntry()
-	input.SetFocused(true)
-	input.SetSizePolicy(tui.Expanding, tui.Maximum)
-
-	input.OnSubmit(func(e *tui.Entry) {
-		if e.Text() != "" {
-			conn.Write([]byte(e.Text() + "\n"))
-		}
-		input.SetText("")
-	})
-
-	inputBox := tui.NewHBox(input)
-	inputBox.SetBorder(true)
-	inputBox.SetSizePolicy(tui.Expanding, tui.Maximum)
-
-	chat := tui.NewVBox(historyBox, inputBox)
-	chat.SetSizePolicy(tui.Expanding, tui.Expanding)
-	root := tui.NewHBox(sidebar, chat)
-
-	ui, err := tui.New(root)
+	g, err := gocui.NewGui(gocui.OutputNormal)
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
+	}
+	defer g.Close()
+	g.Cursor = true
+	g.Mouse = true
+	g.SetManagerFunc(layout)
+	initKeyBindings(g)
+	if err := g.SetKeybinding("input", gocui.KeyEnter, gocui.ModNone,
+		func(g *gocui.Gui, v *gocui.View) error {
+			b := make([]byte, len(v.Buffer()))
+			n, err := v.Read(b)
+			if n != len(v.Buffer()) {
+				return fmt.Errorf("mismatch between buffer length and bytes read")
+			}
+			if err != nil && err != io.EOF {
+				return err
+			}
+			conn.Write(b)
+			displayMessage(g, "chat", b)
+			v = g.CurrentView()
+			v.Clear()
+			x0, y0 := v.Origin()
+			x, y := v.Cursor()
+			dx, dy := x0-x, y0-y
+			v.MoveCursor(dx, dy, true)
+			return nil
+
+		}); err != nil {
+		panic(err)
 	}
 
-	ui.SetKeybinding("Esc", func() { ui.Quit() })
+	if err := g.SetKeybinding("input", gocui.KeyArrowUp, gocui.ModNone,
+		func(g *gocui.Gui, v *gocui.View) error {
+			g.SetCurrentView("chat")
+			v, _ = g.View("chat")
 
-	go receiveMessage(conn, history)
-	if err := ui.Run(); err != nil {
-		log.Fatal(err)
+			return nil
+		}); err != nil {
+		panic(err)
+	}
+
+	go receiveMessage(conn, g)
+	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
+		panic(err)
 	}
 }
